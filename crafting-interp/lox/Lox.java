1552d9a10c9abab155b6542f6aa2226e07a4a39a
Md Ali <mali54@hawk.iit.edu>
2022-01-05 21:36:42 -0600

    Lox.java file update with runnable makefile

--- crafting-interp/lox/Lox.java
+++ crafting-interp/lox/Lox.java
@@ -8,8 +8,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List; 
 
-public class Lox { 
-    public static void main(String[] args) throw IOException { 
+public class Lox {
+    static boolean hadError = false; 
+    public static void main(String[] args) throws IOException { 
         if(args.length > 1) { 
             System.out.println("Useage: jlox [script]"); 
             System.exit(64); 
@@ -24,6 +25,48 @@ public class Lox {
 private static void runFile(string path) throws IOException { 
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
+    if(hadError) System.exit(65); 
 }
+
+private static void runPrompt() throws IOException { 
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input); 
+
+    for(;;) { 
+        System.out.print("> "); 
+        String line = reader.readLine(); 
+        if(line == null) { 
+            break; 
+        }
+        run(line);
+        hasError = false; 
+    }
+}
+
+// This is not super useful yet since it only prints out tokens what our scanner would emit. 
+private static void run(String source) { 
+    Scanner scanner = new Scanner(source); 
+    List<Token> tokens = scanner.scanTokens(); 
+
+    // For now, just print the tokens. 
+    for(Token token : tokens) { 
+       System.out.println(token); 
+    }
+}
+
+// Error handling is more of a practical thing instead of a computer science problem. 
+// This gives the user all the information they need to understand
+// We're sticking to line numbers as finding exactly where the error is isn't technically 
+// difficult but just tedious.
+static void error(int line, String message) { 
+    report(line, "", message); 
+}
+
+private static void report(int line, String where, String message) { 
+    System.err.println( "[line " + line + "] Error" + where + ": " + message); 
+    hadError = true; 
+}
+
+
 
 
