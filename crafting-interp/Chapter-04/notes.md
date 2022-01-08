f93b298211fd92c69a6f971f11ac115ae87a85ad
Md Ali <mali54@hawk.iit.edu>
2022-01-08 15:22:08 -0600

    Scanner complete, fix makefile

--- crafting-interp/Chapter-04/notes.md
+++ crafting-interp/Chapter-04/notes.md
@@ -8,4 +8,25 @@ The core of the scanner is a loop. Starting with the first character of the sour
 
 There is tools to automatically do this and there is similaries into theory of computation as well as some PL thoery but we won't get into that or use the tools and will handcraft the scanner. 
 
-Start at page 48
+So now at this point our scanner can handle a few things such as this 
+// this is a comment
+(()) {} // Grouping things 
+// Even all basic operators
+
+At this point we are able to handle like one character at this point for our scanner or things like != where we have to see if the next character is an equal sign. Now we should be able to handle string literals. 
+
+In our Lox langauge we have that all numbers are floating points at runtime but it supports both integeres and decimal literals. So what the fuck is a number literal you might ask? A number literal is a series of digits optionally followed by a decimal point and one more trailing digits: 
+
+1234
+12.34 
+
+We won't allow leading or trailing decimal points so theses are both invalid: 
+
+.1234
+1234. 
+
+Maximal munch means we can't easily detect a reserved word until we get to the end for example in the Lox language we have that "or" where we can come across something like "orchild" this will obviously casue an error. 
+
+So our goal here is to match "orchild" as an identifier and "or" as a keyword. 
+
+This is where we come to the notion of a reserved word. So a reserved word is an identifer, it's just one that was claimed by the language for it's own use. 
--- crafting-interp/lox/Scanner.java
+++ crafting-interp/lox/Scanner.java
@@ -14,6 +14,28 @@ class Scanner {
 	private int current = 0; 
 	private int line = 1; 
 
+	private static final Map<String, TokenType> keywords; 
+
+	static { 
+		keywords = new HashMap<>();
+		keywords.put("and", AND); 
+		keywords.put("class", CLASS); 
+		keywords.put("else", ELSE); 
+		keywords.put("false", FALSE); 
+		keywords.put("for", FOR); 
+		keywords.put("fun", FUN); 
+		keywords.put("if", IF); 
+		keywords.put("nil", NIL);
+		keywords.put("or", OR);
+		keywords.put("print", PRINT);
+		keywords.put("return", RETURN);
+		keywords.put("super", SUPER); 
+		keywords.put("this", THIS);
+		keywords.put("true", TRUE); 
+		keywords.put("var", VAR);
+		keywords.put("while", WHILE); 
+	}
+
 	Scanner(String source) { 
 		this.source = source;
 	}
@@ -67,12 +89,88 @@ class Scanner {
 				  } else { 
 					  addToken(SLASH); 
 				  }
+				  break;
+			case ' ':
+			case '\r': 
+			case '\t':
+				 // When encounting white space like this we simiply go back to the beginning of the scan loop.  
+				  break; 
+			case '\n':
+			  	  line++; 
+				  break; 
+			case '"': string(); 
 				  break; 
 
-			default: 
+			case 'o': 
+				  if(peek() == 'r') { 
+					addToken(OR); 
+				  }
+				  break; 
+
+			default:
+				if(isDigit(c)) { 
+					number();
+				} 
+				else if(isAlpha(c)) { 
+					identifier(); 
+				}
+				else {	
 				Lox.error(line, "Unexpected character.");
+				}
 				  break; 
 		}
+	}
+
+	private void identifier() { 
+		while(isAlphaNumberic(peek())) { 
+			advance(); 
+		}
+		
+		String text = source.substring(start, current);
+		TokenType type = keywords.get(text); 
+		if(type == null) { 
+			type = IDENTIFIER;
+		}
+		addToken(type); 
+	}
+
+	private void number() { 
+		while(isDigit(peek())) {
+			advance(); 
+		}
+
+		// Looking for a fraction part. 
+		if(peek() == '.' && isDigit(peekNext())) { 
+			//Consume the . 
+			advance(); 
+
+			while(isDigit(peek())) {
+				advance(); 
+			}
+		}
+
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+
+	private void string() { 
+		while(peek() != '"' && !isAtEnd()) {
+			if(peek() == '\n') {
+				line++;
+			}
+		advance(); 
+		}
+
+		if(isAtEnd()) { 
+			Lox.error(line, "unterminated string.");
+			return; 
+		}
+
+		// The closing ".
+		advance();
+
+		// Trim the surrounding quotes. 
+		String value = source.substring(start + 1, current - 1);
+		addToken(STRING, value);
 	}
 
 	private boolean match(char expected) { 
@@ -96,6 +194,28 @@ class Scanner {
 		}
 
 		return source.charAt(current); 
+	}
+
+	private char peekNext() { 
+		if(current + 1 >= source.length()) { 
+			return '\0';
+		}
+
+		return source.charAt(current + 1); 
+	}
+
+	private boolean isAlpha(char c) { 
+		return(c >= 'a' && c <= 'z') || 
+		      (c >= 'A' && c <= 'Z') || 
+	      	       c == '_';	      
+	}
+
+	private boolean isAlphaNumeric(char c) { 
+		return isAlpha(c) || isDigit(c); 
+	}
+
+	private boolean isDigit(char c) { 
+		return c >= '0' && c <= '9';
 	}
 
 	// Helper fucntions that helps us if we've consumed all the char
